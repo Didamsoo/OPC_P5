1d640d389be4d2a605ed3cfcb2156773c3b42b29
Didamsoo <ltueesday.adam@gmail.com>
2024-08-14 14:03:37 +0200

    Init Projet + Ajout de React Router + création des composants, intégration des routes dans app.js + Ajout fichier Layout.js

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home'; 
+import About from './About'; 
+import NotFound from './NotFound'; 
+import Layout from './Layout';
+
+function App() {
+    return (
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </Router>
+    );
+  }
+  
+  export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>KASA</h1>;
+  }
+  
+  export default Home;
+  
